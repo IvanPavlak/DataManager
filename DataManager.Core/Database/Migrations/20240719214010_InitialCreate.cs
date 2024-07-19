6ffcdebecfc5b8bdf1cffbf4145edbe07b3070a1
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-19 23:47:44 +0200

    Add initial migration

---
+++ DataManager.Core/Database/Migrations/20240719214010_InitialCreate.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataManager.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataModelOnes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ExitId = table.Column<int>(type: "integer", nullable: false),
+                    Port = table.Column<string>(type: "text", nullable: false),
+                    UserGroup = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    MemberId = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    GainAmountOne = table.Column<int>(type: "integer", nullable: false),
+                    GainAmountTwo = table.Column<int>(type: "integer", nullable: false),
+                    Loss = table.Column<int>(type: "integer", nullable: false),
+                    Total = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataModelOnes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DataModelOnes_Exits_ExitId",
+                        column: x => x.ExitId,
+                        principalTable: "Exits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataModelTwos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ExitId = table.Column<int>(type: "integer", nullable: false),
+                    PeriodStartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    PeriodEndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    GainAmountThree = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataModelTwos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DataModelTwos_Exits_ExitId",
+                        column: x => x.ExitId,
+                        principalTable: "Exits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataModelOnes_ExitId",
+                table: "DataModelOnes",
+                column: "ExitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataModelTwos_ExitId",
+                table: "DataModelTwos",
+                column: "ExitId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataModelOnes");
+
+            migrationBuilder.DropTable(
+                name: "DataModelTwos");
+
+            migrationBuilder.DropTable(
+                name: "Exits");
+        }
+    }
+}
