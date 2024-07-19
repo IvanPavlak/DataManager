b8cee250ba2ae636bf243fb0750ead645b9d8f40
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-19 11:20:54 +0200

    Add DbValidation and DBModel validators

---
+++ DataManager.Core/DbValidation/DataModelOneValidator.cs
@@ -0,0 +1,46 @@
+using DataManager.Core.DBModels;
+using FluentValidation;
+
+namespace DataManager.Core.DbValidation;
+
+public class DataModelOneValidator : AbstractValidator<DataModelOne>
+{
+    public DataModelOneValidator()
+    {
+        RuleFor(d => d.ExitId)
+            .NotEmpty();
+
+        RuleFor(d => d.Exit)
+            .NotEmpty();
+
+        RuleFor(d => d.Port)
+            .NotEmpty();
+
+        RuleFor(d => d.UserGroup)
+            .NotEmpty();
+
+        RuleFor(d => d.Country)
+            .NotEmpty();
+
+        RuleFor(d => d.MemberId)
+            .NotEmpty();
+
+        RuleFor(d => d.Date)
+            .NotEmpty()
+
+            .Must(date => DbValidation.CheckIsValidDate(date, "yyyy-MM-dd"))
+            .WithMessage("MeasurementDate must be in this format: yyyy-MM-dd");
+
+        RuleFor(d => d.GainAmountOne)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(d => d.GainAmountTwo)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(d => d.Loss)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(d => d.Total)
+            .GreaterThanOrEqualTo(0);
+    }
+}
---
+++ DataManager.Core/DbValidation/DataModelTwoValidator.cs
@@ -0,0 +1,29 @@
+using DataManager.Core.DBModels;
+using FluentValidation;
+
+namespace DataManager.Core.DbValidation;
+
+public class DataModelTwoValidator : AbstractValidator<DataModelTwo>
+{
+    public DataModelTwoValidator()
+    {
+        RuleFor(d => d.ExitId)
+            .NotEmpty();
+
+        RuleFor(d => d.PeriodStartDate)
+            .NotEmpty()
+
+            .Must(date => DbValidation.CheckIsValidDate(date))
+            .WithMessage("PeriodStartDate must be in this format: dd.MM.yyyy.");
+
+        RuleFor(d => d.PeriodEndDate)
+            .NotEmpty()
+            .WithMessage("PeriodEndDate must not be empty!")
+
+            .Must(date => DbValidation.CheckIsValidDate(date))
+            .WithMessage("PeriodEndDate must be in this format: dd.MM.yyyy. HH:mm:ss")
+
+            .GreaterThan(d => d.PeriodStartDate.ToDateTime(new TimeOnly(0, 0, 0)))
+            .WithMessage("PeriodEndDate must be greater than PeriodStartDate!");
+    }
+}
