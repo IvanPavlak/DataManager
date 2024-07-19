331f9a5d7a475ed18fe6e9eff939a8dacc751f30
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-19 23:47:16 +0200

    Add DataManager.Console

--- DataManager.Console/Program.cs
+++ DataManager.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataManager.Console;
+using DataManager.Core;
+using MetForecasting.BDM.Core;
+using Spectre.Console;
+
+using (var dbContext = new DataManagerDbContext())
+{
+    var dataManagerService = new DataManagerService(dbContext);
+
+    PrettifyConsole.Title("MetForecastingBDM");
+
+    AnsiConsole.MarkupLine($"[bold red]-> DELETING EXISTING DATABASE! FOR TESTING ONLY! <-[/]");
+    dbContext.Database.EnsureDeleted();
+    PrettifyConsole.CreateAndDisplayLine("dodgerblue1");
+
+    string csvFilePath = Path.Combine("E:/VSCode/GitHub/Data_Manager/DataManager.Core/Database/Data/DataModelOne_1k_rows.csv");
+    string xlsxFilePath = Path.Combine("E:/VSCode/GitHub/Data_Manager/DataManager.Core/Database/Data/DataModelOne_1k_rows.xlsx");
+
+    DataManagerPrompts.ConsoleAppStartPrompt(dataManagerService, csvFilePath, xlsxFilePath, dbContext);
+}
