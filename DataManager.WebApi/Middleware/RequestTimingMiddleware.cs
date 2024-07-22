d9403d0571917d6990180bc8d8674dabfa1a724f
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-22 14:45:12 +0200

    Add RequestTimingMiddleware

--- DataManager.WebApi/Middleware/RequestTimingMiddleware.cs
+++ DataManager.WebApi/Middleware/RequestTimingMiddleware.cs
@@ -1,12 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace DataManager.WebApi.Middleware
+namespace DataManager.WebApi.Middleware;
+
+public class RequestTimingMiddleware
 {
-    public class RequestTimingMiddleware
+    private readonly RequestDelegate next;
+    private readonly ILogger<RequestTimingMiddleware> logger;
+
+    public RequestTimingMiddleware(RequestDelegate next, ILogger<RequestTimingMiddleware> logger)
     {
-        
+        this.next = next;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stopWatch = new Stopwatch();
+
+        try
+        {
+            stopWatch.Start();
+            await next(context);
+        }
+        finally
+        {
+            stopWatch.Stop();
+
+            var elapsedMilliseconds = stopWatch.ElapsedMilliseconds;
+            logger.LogInformation("{RequestMethod} {RequestPath} request took {EllapsedMilliseconds}ms to complete",
+            context.Request.Method,
+            context.Request.Path,
+            elapsedMilliseconds);
+        }
     }
 }
