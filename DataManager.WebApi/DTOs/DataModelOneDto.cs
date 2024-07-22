9c097b878b0f0d87373572484e9509edf3e5ed22
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-22 14:44:22 +0200

    Add DTOs

--- DataManager.WebApi/DTOs/DataModelOneDto.cs
+++ DataManager.WebApi/DTOs/DataModelOneDto.cs
@@ -1,12 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace DataManager.WebApi.DTOs;
 
-namespace DataManager.WebApi.DTOs
-{
-    public class DataModelOneDto
-    {
-        
-    }
-}
+public record GetDataModelOneDto(
+    int PageNumber = 1,
+    int PageSize = 1,
+    string Filter = null
+);
+
+public record GetDataModelOneDtoV1(
+    int ExitId,
+    string Port,
+    string UserGroup,
+    string Country,
+    int MemberId,
+    DateOnly Date,
+    int GainAmountOne,
+    int GainAmountTwo,
+    int Loss,
+    int Total
+);
+
+public record CreateDataModelOneDto(
+    int ExitId,
+    string Port,
+    string UserGroup,
+    string Country,
+    int MemberId,
+    DateOnly Date,
+    int GainAmountOne,
+    int GainAmountTwo,
+    int Loss,
+    int Total
+);
+
+public record UpdateDataModelOneDto(
+    string Port,
+    string UserGroup,
+    string Country,
+    int MemberId,
+    DateOnly Date,
+    int GainAmountOne,
+    int GainAmountTwo,
+    int Loss,
+    int Total
+);
--- DataManager.WebApi/DTOs/DataModelTwoDto.cs
+++ DataManager.WebApi/DTOs/DataModelTwoDto.cs
@@ -1,12 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace DataManager.WebApi.DTOs;
 
-namespace DataManager.WebApi.DTOs
-{
-    public class DataModelTwoDto
-    {
-        
-    }
-}
+public record GetDataModelTwoDto(
+    int PageNumber = 1,
+    int PageSize = 1,
+    string Filter = null
+);
+
+public record GetDataModelTwoDtoV1(
+    int ExitId,
+    DateOnly PeriodStartDate,
+    DateTime PeriodEndDate,
+    int GainAmountThree
+);
+
+public record CreateDataModelTwoDto(
+    int ExitId,
+    DateOnly PeriodStartDate,
+    DateTime PeriodEndDate,
+    int GainAmountThree
+);
+
+public record UpdateDataModelTwoDto(
+    DateOnly PeriodStartDate,
+    DateTime PeriodEndDate,
+    int GainAmountThree
+);
