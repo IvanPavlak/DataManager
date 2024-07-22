376249b3b5bd6b5f137f04b91db820eb6dbe16eb
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-22 14:44:34 +0200

    Add EntityExtensions

--- DataManager.WebApi/DTOs/EntityExtensions.cs
+++ DataManager.WebApi/DTOs/EntityExtensions.cs
@@ -1,12 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DataManager.Core.DBModels;
 
-namespace DataManager.WebApi.DTOs
+namespace DataManager.WebApi.DTOs;
+
+public static class EntityExtensions
 {
-    public class EntityExtensions
+    public static GetDataModelOneDtoV1 AsDtoV1(this DataModelOne dataModelOne)
     {
-        
+        return new GetDataModelOneDtoV1(
+            dataModelOne.ExitId,
+            dataModelOne.Port,
+            dataModelOne.UserGroup,
+            dataModelOne.Country,
+            dataModelOne.MemberId,
+            dataModelOne.Date,
+            dataModelOne.GainAmountOne,
+            dataModelOne.GainAmountTwo,
+            dataModelOne.Loss,
+            dataModelOne.Total
+
+        );
+    }
+
+    public static GetDataModelTwoDtoV1 AsDtoV1(this DataModelTwo dataModelTwo)
+    {
+        return new GetDataModelTwoDtoV1(
+            dataModelTwo.ExitId,
+            dataModelTwo.PeriodStartDate,
+            dataModelTwo.PeriodEndDate,
+            dataModelTwo.GainAmountThree
+        );
     }
 }
