72f2b44c30d59ecc7d9ae98f77fe7acfe61a2cf9
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-22 14:45:01 +0200

    Add HttpResponseExtensions

--- DataManager.WebApi/Endpoints/HttpResponseExtensions.cs
+++ DataManager.WebApi/Endpoints/HttpResponseExtensions.cs
@@ -1,12 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text.Json;
 
-namespace DataManager.WebApi.Endpoints
+namespace DataManager.WebApi.Endpoints;
+
+public static class HttpResponseExtensions
 {
-    public class HttpResponseExtensions
+    public static void AddPaginationHeader(this HttpResponse response, int totalCount, int pageSize)
     {
-        
+        var paginationHeader = new
+        {
+            totalPages = (int)Math.Ceiling(totalCount / (double)pageSize)
+        };
+
+        response.Headers.Append("X-Pagination", JsonSerializer.Serialize(paginationHeader));
     }
 }
