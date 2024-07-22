da788b290eb09e0822cac44137905adb935da966
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-22 14:44:09 +0200

    Add DataExtensions

--- DataManager.WebApi/Data/DataExtensions.cs
+++ DataManager.WebApi/Data/DataExtensions.cs
@@ -1,12 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DataManager.Core;
+using DataManager.WebApi.Repositories;
+using Microsoft.EntityFrameworkCore;
 
-namespace DataManager.WebApi.Data
+namespace DataManager.WebApi.Data;
+
+public static class DataExtensions
 {
-    public class DataExtensions
+    public static async Task InitializeDbAsync(this IServiceProvider serviceProvider)
     {
-        
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<DataManagerDbContext>();
+        await dbContext.Database.MigrateAsync();
+
+        var logger = serviceProvider.GetRequiredService<ILoggerFactory>()
+                                    .CreateLogger("DB initializer");
+
+        logger.LogInformation(5, "The database is ready!");
+    }
+
+    public static IServiceCollection AddRepositories(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var connectionString = configuration.GetConnectionString("ConnectionString");
+        services.AddNpgsql<DataManagerDbContext>(connectionString)
+                .AddScoped<IDataModelOnesRepository, EntityFrameworkRepository>()
+                .AddScoped<IDataModelTwosRepository, EntityFrameworkRepository>();
+
+        return services;
     }
 }
