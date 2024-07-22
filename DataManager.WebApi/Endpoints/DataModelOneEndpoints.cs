5abc6a6c21475d42486b0305f919f4287db1feb5
Ivan Pavlak <ivanpavlak00@gmail.com>
2024-07-22 14:44:45 +0200

    Add endpoints

--- DataManager.WebApi/Endpoints/DataModelOneEndpoints.cs
+++ DataManager.WebApi/Endpoints/DataModelOneEndpoints.cs
@@ -1,12 +1,225 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using DataManager.Core.DBModels;
+using DataManager.WebApi.Authorization;
+using DataManager.WebApi.DTOs;
+using DataManager.WebApi.Repositories;
 
-namespace DataManager.WebApi.Endpoints
+namespace DataManager.WebApi.Endpoints;
+
+public static class DataModelOnesEndpoints
 {
-    public class DataModelOneEndpoints
+    const string GetDataModelOneV1EndpointName = "GetDataModelOne";
+
+    public static RouteGroupBuilder MapDataModelOnesEndpoints(this IEndpointRouteBuilder routes)
     {
-        
+        var group = routes.NewVersionedApi()
+                          .MapGroup("/dataModelOnes")
+                          .HasApiVersion(1.0);
+
+        group.MapGet("/", async (
+            IDataModelOnesRepository repository,
+            ILoggerFactory loggerFactory,
+            [AsParameters] GetDataModelOneDto request,
+            HttpContext http) =>
+            {
+                var totalCount = await repository.CountDataModelOneAsync(request.Filter);
+                http.Response.AddPaginationHeader(totalCount, request.PageSize);
+
+                var logger = loggerFactory.CreateLogger("DataModelOnes Endpoints");
+
+                try
+                {
+                    var dataModelOnes = await repository.GetAllDataModelOnesAsync(request.PageNumber, request.PageSize, request.Filter);
+                    logger.LogInformation("Retrieved all dataModelOnes successfully.");
+                    return Results.Ok(dataModelOnes.Select(dataModelOne => dataModelOne.AsDtoV1()));
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                        Environment.MachineName,
+                        Activity.Current?.TraceId);
+
+                    return Results.Problem(
+                        title: "Working on it!",
+                        statusCode: StatusCodes.Status500InternalServerError,
+                        extensions: new Dictionary<string, object>
+                        {
+                            {"traceId", Activity.Current?.TraceId.ToString()}
+                        }
+                    );
+                }
+            })
+            .RequireAuthorization(Policies.ReadAccess)
+            .MapToApiVersion(1.0);
+
+        group.MapGet("/{id}", async (IDataModelOnesRepository repository, ILoggerFactory loggerFactory, int id) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelOnes Endpoints");
+
+            try
+            {
+                var dataModelOne = await repository.GetDataModelOneAsync(id);
+                if (dataModelOne is not null)
+                {
+                    logger.LogInformation("Retrieved DataModelOne with ID {Id} successfully.", id);
+                    return Results.Ok(dataModelOne.AsDtoV1());
+                }
+                else
+                {
+                    logger.LogWarning("DataModelOne with ID {Id} not found.", id);
+                    return Results.NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .WithName(GetDataModelOneV1EndpointName)
+        .RequireAuthorization(Policies.ReadAccess)
+        .MapToApiVersion(1.0); ;
+
+        group.MapPost("/", async (IDataModelOnesRepository repository, ILoggerFactory loggerFactory, CreateDataModelOneDto dataModelOneDto) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelOnes Endpoints");
+
+            try
+            {
+                var dataModelOne = new DataModelOne
+                {
+                    ExitId = dataModelOneDto.ExitId,
+                    Port = dataModelOneDto.Port,
+                    UserGroup = dataModelOneDto.UserGroup,
+                    Country = dataModelOneDto.Country,
+                    MemberId = dataModelOneDto.MemberId,
+                    Date = dataModelOneDto.Date,
+                    GainAmountOne = dataModelOneDto.GainAmountOne,
+                    GainAmountTwo = dataModelOneDto.GainAmountTwo,
+                    Loss = dataModelOneDto.Loss,
+                    Total = dataModelOneDto.Total
+                };
+
+                await repository.CreateDataModelOneAsync(dataModelOne);
+                logger.LogInformation("Created a new dataModelOne with ID {Id} successfully.", dataModelOne.Id);
+                return Results.CreatedAtRoute(GetDataModelOneV1EndpointName, new { id = dataModelOne.Id }, dataModelOne);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .RequireAuthorization(Policies.WriteAccess)
+        .MapToApiVersion(1.0); ;
+
+        group.MapPut("/{id}", async (IDataModelOnesRepository repository, ILoggerFactory loggerFactory, int id, UpdateDataModelOneDto updatedDataModelOneDto) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelOnes Endpoints");
+
+            try
+            {
+                var existingDataModelOne = await repository.GetDataModelOneAsync(id);
+
+                if (existingDataModelOne is null)
+                {
+                    logger.LogWarning("DataModelOne with ID {Id} not found.", id);
+                    return Results.NotFound();
+                }
+
+                existingDataModelOne.Port = updatedDataModelOneDto.Port;
+                existingDataModelOne.UserGroup = updatedDataModelOneDto.UserGroup;
+                existingDataModelOne.Country = updatedDataModelOneDto.Country;
+                existingDataModelOne.MemberId = updatedDataModelOneDto.MemberId;
+                existingDataModelOne.Date = updatedDataModelOneDto.Date;
+                existingDataModelOne.GainAmountOne = updatedDataModelOneDto.GainAmountOne;
+                existingDataModelOne.GainAmountTwo = updatedDataModelOneDto.GainAmountTwo;
+                existingDataModelOne.Loss = updatedDataModelOneDto.Loss;
+                existingDataModelOne.Total = updatedDataModelOneDto.Total;
+
+                await repository.UpdateDataModelOneAsync(existingDataModelOne);
+                logger.LogInformation("Updated DataModelOne with ID {Id} successfully.", id);
+
+                return Results.NoContent();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .RequireAuthorization(Policies.WriteAccess)
+        .MapToApiVersion(1.0); ;
+
+        group.MapDelete("/{id}", async (IDataModelOnesRepository repository, ILoggerFactory loggerFactory, int id) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelOnes Endpoints");
+
+            try
+            {
+                var dataModelOne = await repository.GetDataModelOneAsync(id);
+
+                if (dataModelOne is not null)
+                {
+                    await repository.DeleteDataModelOneAsync(id);
+                    logger.LogInformation("Deleted DataModelOne with ID {Id} successfully.", id);
+                }
+                else
+                {
+                    logger.LogWarning("DataModelOne with ID {Id} not found.", id);
+                }
+
+                return Results.NoContent();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .RequireAuthorization(Policies.WriteAccess)
+        .MapToApiVersion(1.0); ;
+
+        return group;
     }
 }
--- DataManager.WebApi/Endpoints/DataModelTwoEndpoints.cs
+++ DataManager.WebApi/Endpoints/DataModelTwoEndpoints.cs
@@ -1,12 +1,212 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using DataManager.Core.DBModels;
+using DataManager.WebApi.Authorization;
+using DataManager.WebApi.DTOs;
+using DataManager.WebApi.Repositories;
 
-namespace DataManager.WebApi.Endpoints
+namespace DataManager.WebApi.Endpoints;
+
+public static class DataModelTwosEndpoints
 {
-    public class DataModelTwoEndpoints
+    const string GetDataModelTwoV1EndpointName = "GetDataModelTwo";
+
+    public static RouteGroupBuilder MapDataModelTwosEndpoints(this IEndpointRouteBuilder routes)
     {
-        
+        var group = routes.NewVersionedApi()
+                          .MapGroup("/dataModelTwos")
+                          .HasApiVersion(1.0);
+
+        group.MapGet("/", async (
+            IDataModelTwosRepository repository,
+            ILoggerFactory loggerFactory,
+            [AsParameters] GetDataModelTwoDto request,
+            HttpContext http) =>
+            {
+                var totalCount = await repository.CountDataModelTwoAsync();
+                http.Response.AddPaginationHeader(totalCount, request.PageSize);
+                var logger = loggerFactory.CreateLogger("DataModelTwos Endpoints");
+
+                try
+                {
+                    var dataModelTwos = await repository.GetAllDataModelTwosAsync(request.PageNumber, request.PageSize);
+                    logger.LogInformation("Retrieved all DataModelTwos successfully.");
+                    return Results.Ok(dataModelTwos.Select(dataModelTwo => dataModelTwo.AsDtoV1()));
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                        Environment.MachineName,
+                        Activity.Current?.TraceId);
+
+                    return Results.Problem(
+                        title: "Working on it!",
+                        statusCode: StatusCodes.Status500InternalServerError,
+                        extensions: new Dictionary<string, object>
+                        {
+                            {"traceId", Activity.Current?.TraceId.ToString()}
+                        }
+                    );
+                }
+            })
+            .RequireAuthorization(Policies.ReadAccess)
+            .MapToApiVersion(1.0);
+
+        group.MapGet("/{id}", async (IDataModelTwosRepository repository, ILoggerFactory loggerFactory, int id) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelTwos Endpoints");
+
+            try
+            {
+                var dataModelTwo = await repository.GetDataModelTwoAsync(id);
+                if (dataModelTwo is not null)
+                {
+                    logger.LogInformation("Retrieved DataModelTwo with ID {Id} successfully.", id);
+                    return Results.Ok(dataModelTwo.AsDtoV1());
+                }
+                else
+                {
+                    logger.LogWarning("DataModelTwo with ID {Id} not found.", id);
+                    return Results.NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .WithName(GetDataModelTwoV1EndpointName)
+        .RequireAuthorization(Policies.ReadAccess)
+        .MapToApiVersion(1.0);
+
+        group.MapPost("/", async (IDataModelTwosRepository repository, ILoggerFactory loggerFactory, CreateDataModelTwoDto dataModelTwoDto) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelTwos Endpoints");
+
+            try
+            {
+                var dataModelTwo = new DataModelTwo
+                {
+                    ExitId = dataModelTwoDto.ExitId,
+                    PeriodStartDate = dataModelTwoDto.PeriodStartDate,
+                    PeriodEndDate = dataModelTwoDto.PeriodEndDate,
+                    GainAmountThree = dataModelTwoDto.GainAmountThree
+                };
+
+                await repository.CreateDataModelTwoAsync(dataModelTwo);
+                logger.LogInformation("Created a new DataModelTwo with ID {Id} successfully.", dataModelTwo.Id);
+                return Results.CreatedAtRoute(GetDataModelTwoV1EndpointName, new { id = dataModelTwo.Id }, dataModelTwo);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .RequireAuthorization(Policies.WriteAccess)
+        .MapToApiVersion(1.0);
+
+        group.MapPut("/{id}", async (IDataModelTwosRepository repository, ILoggerFactory loggerFactory, int id, UpdateDataModelTwoDto updatedDataModelTwoDto) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelTwos Endpoints");
+
+            try
+            {
+                var existingDataModelTwo = await repository.GetDataModelTwoAsync(id);
+
+                if (existingDataModelTwo is null)
+                {
+                    logger.LogWarning("DataModelTwo with ID {Id} not found.", id);
+                    return Results.NotFound();
+                }
+
+                existingDataModelTwo.PeriodStartDate = updatedDataModelTwoDto.PeriodStartDate;
+                existingDataModelTwo.PeriodEndDate = updatedDataModelTwoDto.PeriodEndDate;
+                existingDataModelTwo.GainAmountThree = updatedDataModelTwoDto.GainAmountThree;
+
+                await repository.UpdateDataModelTwoAsync(existingDataModelTwo);
+                logger.LogInformation("Updated DataModelTwo with ID {Id} successfully.", id);
+
+                return Results.NoContent();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .RequireAuthorization(Policies.WriteAccess)
+        .MapToApiVersion(1.0);
+
+        group.MapDelete("/{id}", async (IDataModelTwosRepository repository, ILoggerFactory loggerFactory, int id) =>
+        {
+            var logger = loggerFactory.CreateLogger("DataModelTwos Endpoints");
+
+            try
+            {
+                var dataModelTwo = await repository.GetDataModelTwoAsync(id);
+
+                if (dataModelTwo is not null)
+                {
+                    await repository.DeleteDataModelTwoAsync(id);
+                    logger.LogInformation("Deleted DataModelTwo with ID {Id} successfully.", id);
+                }
+                else
+                {
+                    logger.LogWarning("DataModelTwo with ID {Id} not found.", id);
+                }
+
+                return Results.NoContent();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName,
+                    Activity.Current?.TraceId);
+
+                return Results.Problem(
+                    title: "Working on it!",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object>
+                    {
+                        {"traceId", Activity.Current?.TraceId.ToString()}
+                    }
+                );
+            }
+        })
+        .RequireAuthorization(Policies.WriteAccess)
+        .MapToApiVersion(1.0);
+
+        return group;
     }
 }
